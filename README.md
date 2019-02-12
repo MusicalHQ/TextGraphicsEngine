bbc375c4baf16c0ae7ea35675d701e7235b9a649
humz <30800959+MusicalHQ@users.noreply.github.com>
2019-02-12 22:34:07 +0000

    cleaning repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# TextGraphicsEngine
-PONG.PY REQUIRES KEYBOARD MODULE
--- text_art.py
+++
@@ -1,6 +0,0 @@
-import text_graphics_engine
-
-screen = text_graphics_engine.screen([40,20]," ")
-h = text_graphics_engine.obj([[10,5],[10,6],[10,7],[10,8],[10,9],[10,10],[10,11],[10,12],[10,13],[11,9],[12,9],[13,5],[13,6],[13,7],[13,8],[13,9],[13,10],[13,11],[13,12],[13,13]],"=",0,screen)
-screen.clear_screen()
-end = input("")
